83aa1eec0f247dc7684cf898fecb35cc35ba3b2d
lightkheehing <lightkheehing@gmail.com>
2019-06-13 18:24:34 +0800

    gelocator(2.1.0)
    - installed without any problems
    - currently testing inside settingPage

--- lib/pages/setting.dart
+++ lib/pages/setting.dart
@@ -1,16 +1,30 @@
 import 'package:flutter/material.dart';
-
+import 'package:sosnyp/main.dart';
+import 'package:geolocator/geolocator.dart';
+import 'dart:async';
 class SettingPage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => _SettingPageState();
 }
 
+var geolocator = Geolocator();
+var locationOptions = LocationOptions(accuracy: LocationAccuracy.high, distanceFilter: 10);
+StreamSubscription<Position> positionStream = geolocator.getPositionStream(locationOptions).listen(
+    (Position position) {
+        print(position == null ? 'Unknown' : position.latitude.toString() + ', ' + position.longitude.toString());
+    });
+testing() async{
+  Position position = await Geolocator().getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+  return position.latitude.toString();
+}
 class _SettingPageState extends State<SettingPage> {
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Flutter Geolocation Example'),
+        resizeToAvoidBottomPadding: false,
+        appBar: new AppBar(
+          title: Text('Profile'),
+          leading: myLeading,
         ),
-        body: Center(child: Text('hi')));
+        body: Center(child: Text(testing().toString())));
   }
 }
