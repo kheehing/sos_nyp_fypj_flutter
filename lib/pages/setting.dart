452687366607875905d7a2b2a2cab2072d90f355
lightkheehing <lightkheehing@gmail.com>
2019-06-13 18:28:55 +0800

    able to get location
     - getting location success !

--- lib/pages/setting.dart
+++ lib/pages/setting.dart
@@ -2,29 +2,62 @@ import 'package:flutter/material.dart';
 import 'package:sosnyp/main.dart';
 import 'package:geolocator/geolocator.dart';
 import 'dart:async';
+import 'package:location/location.dart';
+
 class SettingPage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => _SettingPageState();
 }
 
-var geolocator = Geolocator();
-var locationOptions = LocationOptions(accuracy: LocationAccuracy.high, distanceFilter: 10);
-StreamSubscription<Position> positionStream = geolocator.getPositionStream(locationOptions).listen(
-    (Position position) {
-        print(position == null ? 'Unknown' : position.latitude.toString() + ', ' + position.longitude.toString());
-    });
-testing() async{
-  Position position = await Geolocator().getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
-  return position.latitude.toString();
-}
 class _SettingPageState extends State<SettingPage> {
+  var location = new Location();
+
+  Map<String, double> userLocation;
   Widget build(BuildContext context) {
     return Scaffold(
         resizeToAvoidBottomPadding: false,
         appBar: new AppBar(
-          title: Text('Profile'),
+          title: Text('Setting'),
           leading: myLeading,
         ),
-        body: Center(child: Text(testing().toString())));
+        body: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            userLocation == null
+                ? CircularProgressIndicator()
+                : Text("Location:" +
+                    userLocation["latitude"].toString() +
+                    " " +
+                    userLocation["longitude"].toString()),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: RaisedButton(
+                onPressed: () {
+                  _getLocation().then((value) {
+                    setState(() {
+                      userLocation = value;
+                    });
+                  });
+                },
+                color: Colors.blue,
+                child: Text(
+                  "Get Location",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+          ],
+        )));
+  }
+
+  Future<Map<String, double>> _getLocation() async {
+    var currentLocation = <String, double>{};
+    try {
+      currentLocation = await location.getLocation();
+    } catch (e) {
+      currentLocation = null;
+    }
+    return currentLocation;
   }
 }
