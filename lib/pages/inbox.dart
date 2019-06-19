50cd32c4328b16502b809766373beb9b926bc376
lightkheehing <lightkheehing@gmail.com>
2019-06-19 16:06:44 +0800

    location working

--- lib/pages/inbox.dart
+++ lib/pages/inbox.dart
@@ -1,10 +1,15 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:sosnyp/main.dart';
 import 'package:intl/intl.dart';
-import 'dart:async';
+import 'package:rflutter_alert/rflutter_alert.dart';
+import 'package:sosnyp/main.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class InboxPage extends StatefulWidget {
   @override
@@ -13,12 +18,6 @@ class InboxPage extends StatefulWidget {
 
 class _InboxPageState extends State<InboxPage> {
   Stream<QuerySnapshot> helpCurrent;
-
-  @override
-  void initState() {
-    super.initState();
-    helpCurrent = Firestore.instance.collection('help.current').snapshots();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,12 +29,13 @@ class _InboxPageState extends State<InboxPage> {
       drawer: new MyDrawer(),
       body: StreamBuilder<QuerySnapshot>(
           stream: helpCurrent,
-          builder: (context, snapshot) {
+          builder:
+              (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
             if (snapshot.hasError) {
               return Center(child: Text('Error: ${snapshot.error}'));
             }
             if (!snapshot.hasData) {
-              return Center(child: Text('Loading...'));
+              return loadingScreen();
             }
             return ListView.builder(
                 // itemExtent: 80,
@@ -48,9 +48,13 @@ class _InboxPageState extends State<InboxPage> {
 
   Widget buildListItem(BuildContext context, DocumentSnapshot document) {
     TextEditingController _controllerName = new TextEditingController();
-    TextEditingController _controllerNameDialog = new TextEditingController();
-    TextEditingController _controllerGenderDialog = new TextEditingController();
-    TextEditingController _controllerAdminDialog = new TextEditingController();
+    Future<dynamic> _getProfile(user) async {
+      DocumentSnapshot querySnapshot =
+          await Firestore.instance.collection("profile").document(user).get();
+      Map<String, dynamic> jsonString = querySnapshot.data;
+      final jsonRespone = json.encode(jsonString.toString());
+      return jsonRespone;
+    }
 
     Firestore.instance
         .collection('profile')
@@ -59,69 +63,89 @@ class _InboxPageState extends State<InboxPage> {
         .then((data) {
       setState(() async {
         String _name = await data['name'];
-        String _gender = await data['gender'];
-        String _admin = await data['admin'];
         _controllerName.text = _name;
-        _controllerNameDialog.text = 'Name:  ' + _name;
-        _controllerGenderDialog.text = 'Sex:  ' + _gender;
-        _controllerAdminDialog.text = 'Admin:  ' + _admin;
       });
     });
 
-    void _showDialog() {
-      showDialog(
-          context: context,
-          builder: (BuildContext context) {
-            return AlertDialog(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(15))),
-                title: Column(
-                  children: <Widget>[
-                    TextField(
-                        controller: _controllerNameDialog,
-                        enabled: false,
-                        decoration: InputDecoration(border: InputBorder.none),
-                        style: TextStyle(
-                            fontSize: 15,
-                            fontFamily: 'black_label',
-                            fontWeight: FontWeight.w900)),
-                    TextField(
-                        controller: _controllerAdminDialog,
-                        enabled: false,
-                        decoration: InputDecoration(border: InputBorder.none),
-                        style: TextStyle(
-                            fontSize: 15,
-                            fontFamily: 'black_label',
-                            fontWeight: FontWeight.w900)),
-                    TextField(
-                        controller: _controllerGenderDialog,
-                        enabled: false,
-                        decoration: InputDecoration(border: InputBorder.none),
-                        style: TextStyle(
-                            fontSize: 15,
-                            fontFamily: 'black_label',
-                            fontWeight: FontWeight.w900)),
-                    Text('Location',
-                        style: TextStyle(
-                            fontSize: 15,
-                            fontFamily: 'black_label',
-                            fontWeight: FontWeight.w900)),
-                    // Row(
-                    //   children: <Widget>[
-                    //     TextField(
-                    //         controller: _controllerGenderDialog,
-                    //         enabled: false,
-                    //         decoration:
-                    //             InputDecoration(border: InputBorder.none),
-                    //         style: TextStyle(
-                    //             fontSize: 15,
-                    //             fontFamily: 'black_label',
-                    //             fontWeight: FontWeight.w900)),
-                    //   ],
-                    // )
-                  ],
-                ));
-          });
+    _openMap() async {
+      final String _long = await document['longitude'];
+      final String _lan = await document['latitude'];
+      // Android
+      var url = 'https://www.google.com/maps/search/?api=1&query=$_lan,$_long';
+      if (Platform.isIOS) {
+        // iOS
+        url = 'http://maps.apple.com/?ll=$_lan,$_long';
+      }
+      if (await canLaunch(url)) {
+        await launch(url);
+      } else {
+        throw 'Could not launch $url';
+      }
+    }
+
+    void _showDialog() async {
+      var _data = await _getProfile(document['user'].toString());
+      int genderI = _data.indexOf('gender');
+      int schoolI = _data.indexOf('school');
+      int mobileI = _data.indexOf('mobile');
+      int nameI = _data.indexOf('name');
+      int adminI = _data.indexOf('admin');
+      int courseI = _data.indexOf('course');
+      int endI = _data.indexOf('}');
+      final String gender = _data.substring(genderI + 8, schoolI - 2);
+      final String school = _data.substring(schoolI + 8, mobileI - 2);
+      final String mobile = _data.substring(mobileI + 8, nameI - 2);
+      final String name = _data.substring(nameI + 6, adminI - 2);
+      final String admin = _data.substring(adminI + 7, courseI - 2);
+      final String course = _data.substring(courseI + 8, endI);
+      // print(gender + school + mobile + name + admin + course + _data);
+      Alert(
+        style: AlertStyle(
+          animationType: AnimationType.fromBottom,
+          isCloseButton: false,
+          isOverlayTapDismiss: true,
+          descStyle: TextStyle(
+              fontSize: 15,
+              fontFamily: 'black_label',
+              fontWeight: FontWeight.w900),
+          animationDuration: Duration(milliseconds: 50),
+          alertBorder: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10.0),
+            side: BorderSide(
+              color: Colors.white,
+            ),
+          ),
+          titleStyle: TextStyle(
+              fontSize: 20,
+              fontFamily: 'black_label',
+              fontWeight: FontWeight.w900),
+        ),
+        context: context,
+        // type: AlertType.warning,
+        title: "$name\n$admin",
+        desc:
+            "Sex: $gender \nMobile: $mobile \nSchool: $school \nCourse: $course",
+        buttons: [
+          DialogButton(
+              child: Text(
+                'Location',
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
+              onPressed: () => _openMap().whenComplete(() {
+                    Navigator.pop(context);
+                  }),
+              color: Colors.blue),
+          DialogButton(
+              child: Text(
+                'Help',
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
+              onPressed: () => _openMap().whenComplete(() {
+                    Navigator.pop(context);
+                  }),
+              color: Colors.green),
+        ],
+      ).show();
     }
 
     return ListTile(
@@ -129,13 +153,16 @@ class _InboxPageState extends State<InboxPage> {
         children: <Widget>[
           Expanded(
               child: TextField(
-                  controller: _controllerName,
-                  enabled: false,
-                  decoration: InputDecoration(border: InputBorder.none),
-                  style: TextStyle(
-                      fontSize: 15,
-                      fontFamily: 'black_label',
-                      fontWeight: FontWeight.w900))),
+            controller: _controllerName,
+            enabled: false,
+            decoration: InputDecoration(
+              border: InputBorder.none,
+            ),
+            style: TextStyle(
+                fontSize: 15,
+                fontFamily: 'black_label',
+                fontWeight: FontWeight.w900),
+          )),
           Container(
             width: 180,
             child: Row(
@@ -153,6 +180,15 @@ class _InboxPageState extends State<InboxPage> {
       onTap: () {
         _showDialog();
       },
+      onLongPress: () {
+        _openMap();
+      },
     );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    helpCurrent = Firestore.instance.collection('help.current').snapshots();
   }
 }
