882f4e6d0600389304166558ef3ef48faaaeade2
lightkheehing <lightkheehing@gmail.com>
2019-06-21 18:53:02 +0800

    not working

--- lib/pages/css_test.dart
+++ lib/pages/css_test.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:sosnyp/main.dart';
+import 'package:image_picker/image_picker.dart';
 
 class TestingPage extends StatefulWidget {
   final String title;
@@ -9,15 +12,48 @@ class TestingPage extends StatefulWidget {
   _TestingPageState createState() => new _TestingPageState();
 }
 
+// class _TestingPageState extends State<TestingPage> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+// appBar: AppBar(
+//   title: Text('Test Page'),
+//   leading: myLeading,
+// ),
+// drawer: new MyDrawer(),
+//         body: Center(child: Text('TestPage')));
+//   }
+// }
+
 class _TestingPageState extends State<TestingPage> {
+  File _image;
+
+  Future getImage() async {
+    var image = await ImagePicker.pickImage(source: ImageSource.gallery);
+
+    setState(() {
+      _image = image;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text('Test Page'),
-          leading: myLeading,
-        ),
-        drawer: new MyDrawer(),
-        body: Center(child: Text('TestPage')));
+    return new Scaffold(
+      appBar: AppBar(
+        title: Text('Image Picker Example'),
+        leading: myLeading,
+      ),
+      drawer: new MyDrawer(),
+      body: new Center(
+        child: _image == null
+            ? new Text('No image selected.')
+            : new Image.file(_image),
+      ),
+      floatingActionButton: new FloatingActionButton(
+        onPressed: getImage,
+        tooltip: 'Pick Image',
+        child: new Icon(Icons.add_a_photo),
+      ),
+    );
   }
 }
